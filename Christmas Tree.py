a58439c8e70e948ebdc818d90befce007d6cb5ae
RakinKhan <47644682+RakinKhan@users.noreply.github.com>
2020-02-11 16:13:51 -0500

    Update Christmas Tree.py

--- Christmas Tree.py
+++ Christmas Tree.py
@@ -8,14 +8,15 @@ picture = [
   [0,0,0,1,0,0,0],
   [0,0,0,1,0,0,0]
 ]
+def christmas_tree():
+  for item in picture:
+    item_row = ''
+    for indiv_item in item:
+      if indiv_item == False:
+        item_row = item_row + ' '
+      else:
+        item_row = item_row + '*'
 
-for item in picture:
-  item_row = ''
-  for indiv_item in item:
-    if indiv_item == False:
-      item_row = item_row + ' '
-    else:
-      item_row = item_row + '*'
-  
-  print(item_row)
+    print(item_row)
+christmas_tree()
 
