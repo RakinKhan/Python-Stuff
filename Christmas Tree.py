331b9ab5edbafd4ae46db153fdb1ce4ebf290318
RakinKhan <47644682+RakinKhan@users.noreply.github.com>
2020-02-11 16:11:31 -0500

    Add files via upload

---
+++ Christmas Tree.py
@@ -0,0 +1,21 @@
+#Exercise!
+#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+
+for item in picture:
+  item_row = ''
+  for indiv_item in item:
+    if indiv_item == False:
+      item_row = item_row + ' '
+    else:
+      item_row = item_row + '*'
+  
+  print(item_row)
+
