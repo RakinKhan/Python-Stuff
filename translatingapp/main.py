acefad64088c39562bb0c6f2082b6f5d468d45c1
RakinKhan <47644682+RakinKhan@users.noreply.github.com>
2020-02-23 01:54:17 -0500

    Update main.py

--- translatingapp/main.py
+++ translatingapp/main.py
@@ -6,7 +6,6 @@ try:
         translator = Translator()
         translations = translator.translate(test.readlines(), dest='ja')
         with open('text-translate.txt', mode='w', encoding='utf8') as newFile:
-            trans = []
             for item in translations:
                 newFile.write(f'{item.origin} -> {item.text}\n')
 except FileNotFoundError:
