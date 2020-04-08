e3faace96722ed4cc5e6ddc9fae8a5ed9f3913f1
RakinKhan <47644682+RakinKhan@users.noreply.github.com>
2020-04-08 18:42:06 -0400

    Add files via upload

---
+++ flask/data.py
@@ -0,0 +1,22 @@
+def Articles():
+    articles = [
+        {'id': 1,
+         'title': 'Article 1',
+         'body': 'lorem',
+         'author': 'Rakin Khan',
+         'create_date': '4-05-27'
+         },
+        {'id': 2,
+         'title': 'Article 2',
+         'body': 'lorem',
+         'author': 'Rakin Khan',
+         'create_date': '4-05-27'
+         },
+        {'id': 3,
+         'title': 'Article 3',
+         'body': 'lorem',
+         'author': 'Rakin Khan',
+         'create_date': '4-05-27'
+         }
+    ]
+    return articles
